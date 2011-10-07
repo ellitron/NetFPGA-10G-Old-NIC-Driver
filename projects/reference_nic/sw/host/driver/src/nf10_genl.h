21fefd8329d3ca914fadfe405ac753e82cfb14a3
Jonathan Ellithorpe <jdellit@stanford.edu>
2011-10-07 08:38:55 -0700

    Updated the headers

--- projects/reference_nic/sw/host/driver/src/nf10_genl.h
+++ projects/reference_nic/sw/host/driver/src/nf10_genl.h
@@ -1,14 +1,41 @@
-/* NetFPGA-10G http://www.netfpga.org
- * 
- * NetFPGA-10G Generic Netlink Definitions
+/*******************************************************************************
  *
- * Description: 
- *  Set of generic netlink definitions that are specific to NetFPGA-10G.
- *  These definitions are used in setting up generic netlink connections
- *  between userspace software and the linux kernel driver.
+ *  NetFPGA-10G http://www.netfpga.org
  *
- * Revision history: 
- *  2011/07/06 Jonathan Ellithorpe: Created.
+ *  File:
+ *        nf10_genl.h
+ *
+ *  Project:
+ *        reference_nic
+ *
+ *  Author:
+ *        Jonathan Ellithorpe
+ *
+ *  Description:
+ *        Set of generic netlink definitions that are specific to NetFPGA-10G.
+ *        These definitions are used in setting up generic netlink connections
+ *        between userspace software and the linux kernel driver.
+ *
+ *  Copyright notice:
+ *        Copyright (C) 2010,2011 The Board of Trustees of The Leland Stanford
+ *                                Junior University
+ *
+ *  Licence:
+ *        This file is part of the NetFPGA 10G development base package.
+ *
+ *        This file is free code: you can redistribute it and/or modify it under
+ *        the terms of the GNU Lesser General Public License version 2.1 as
+ *        published by the Free Software Foundation.
+ *
+ *        This package is distributed in the hope that it will be useful, but
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *        Lesser General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Lesser General Public
+ *        License along with the NetFPGA source package.  If not, see
+ *        http://www.gnu.org/licenses/.
+ *
  */
 
 #ifndef NF10_GENL_H
--- projects/reference_nic/sw/host/driver/src/nf10_reg_lib.c
+++ projects/reference_nic/sw/host/driver/src/nf10_reg_lib.c
@@ -1,23 +1,49 @@
-/* NetFPGA-10G http://www.netfpga.org
- * 
- * NetFPGA-10G Register Access Library
+/*******************************************************************************
  *
- * Description: 
- *  A library of functions for reading and writing registers in the
- *  hardware. These functions read and write registers by invoking
- *  register access functions in the linux driver over a generic netlink
- *  interface. Therefore, the driver must be loaded in the kernel for
- *  these functions to work. Please refer to the wiki for detailed
- *  documentation.
+ *  NetFPGA-10G http://www.netfpga.org
  *
- * Notes:
- *  This library makes use of the libnl-3.0 library for generic netlink
- *  functions. You must install this library into your system before
- *  compiling (http://www.infradead.org/~tgr/libnl/).
+ *  File:
+ *        nf10_reg_lib.c
  *
- * Revision history: 
- *  2011/07/06 Jonathan Ellithorpe: "Let there be a register access
- *                                  library..."
+ *  Project:
+ *        reference_nic
+ *
+ *  Author:
+ *        Jonathan Ellithorpe
+ *
+ *  Description:
+ *         A library of functions for reading and writing registers in the
+ *         hardware. These functions read and write registers by invoking
+ *         register access functions in the linux driver over a generic netlink
+ *         interface. Therefore, the driver must be loaded in the kernel for
+ *         these functions to work. Please refer to the wiki for detailed
+ *         documentation.
+ *
+ *        Notes:
+ *         This library makes use of the libnl-3.0 library for generic netlink
+ *         functions. You must install this library into your system before
+ *         compiling (http://www.infradead.org/~tgr/libnl/).
+ *
+ *  Copyright notice:
+ *        Copyright (C) 2010,2011 The Board of Trustees of The Leland Stanford
+ *                                Junior University
+ *
+ *  Licence:
+ *        This file is part of the NetFPGA 10G development base package.
+ *
+ *        This file is free code: you can redistribute it and/or modify it under
+ *        the terms of the GNU Lesser General Public License version 2.1 as
+ *        published by the Free Software Foundation.
+ *
+ *        This package is distributed in the hope that it will be useful, but
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *        Lesser General Public License for more details.
+ *
+ *        You should have received a copy of the GNU Lesser General Public
+ *        License along with the NetFPGA source package.  If not, see
+ *        http://www.gnu.org/licenses/.
+ *
  */
 
 /* Header files provided by libnl (probably in /usr/local/include). */
